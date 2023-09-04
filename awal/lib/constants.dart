75d3940845bd489d0f797809e07edafd88792c3c
Ali Jasim <ali.jasim.1203@gmail.com>
2023-09-04 16:47:17 +0300

    added the primary and secondry colors ad constants

---
+++ awal/lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromRGBO(0, 168, 225, 100);
+
+const kPrimaryColorDark = Color.fromRGBO(32, 28, 111, 100);
+
+const kSecondryColor = Color.fromRGBO(119, 129, 123, 100);
+
+const kSecondryColorDark = Colors.black;
