c0b8ea3e50abc43fe64a35b7f4569f177f8696cc
Ali Jasim <ali.jasim.1203@gmail.com>
2023-09-05 19:10:18 +0300

    added the card class with 6 teasitng instances

--- lib/ProductCard.dart
+++ lib/ProductCard.dart
@@ -1,4 +1,5 @@
 import 'package:awal/models/productGategory.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
@@ -9,36 +10,42 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: SizedBox(
-        width: double.infinity,
-        height: 200,
-        child: Row(children: [
-          SizedBox(
-            width: 220,
-            height: double.infinity,
-            child: FittedBox(
-              fit: BoxFit.fill,
-              child: product.image,
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushReplacement(
+            context, CupertinoPageRoute(builder: (context) => Container()));
+      },
+      child: Card(
+        child: SizedBox(
+          width: double.infinity,
+          height: 200,
+          child: Row(children: [
+            SizedBox(
+              width: 220,
+              height: double.infinity,
+              child: FittedBox(
+                fit: BoxFit.fill,
+                child: product.image,
+              ),
             ),
-          ),
-          SizedBox(
-            width: 40,
-            height: double.infinity,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  product.title,
-                  style: TextStyle(fontSize: 20),
-                ),
-                Text(product.descreptoin)
-              ],
+            SizedBox(
+              width: 40,
+              height: double.infinity,
             ),
-          )
-        ]),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    product.title,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(product.descreptoin)
+                ],
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
