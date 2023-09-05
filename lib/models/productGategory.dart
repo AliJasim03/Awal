10a60b6d32dda951b5a7d8d828b2fba90b18c0d9
Ali Jasim <ali.jasim.1203@gmail.com>
2023-09-05 17:45:07 +0300

    added then images with class for the produc page

---
+++ lib/models/productGategory.dart
@@ -0,0 +1,10 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class Producds {
+  Image image;
+  String title;
+  String descreptoin;
+  Producds(this.image, this.title, this.descreptoin);
+}
--- lib/products_page.dart
+++ lib/products_page.dart
@@ -1,30 +1,61 @@
-import 'package:flutter/material.dart';
+// import 'package:awal/models/productGategory.dart';
+// import 'package:flutter/material.dart';
 
-class ProductsPage extends StatefulWidget {
-  const ProductsPage({super.key});
+// class ProductsPage extends StatefulWidget {
+//   const ProductsPage({super.key});
 
-  @override
-  State<ProductsPage> createState() => _ProductsPageState();
-}
+//   @override
+//   State<ProductsPage> createState() => _ProductsPageState();
+// }
 
-class _ProductsPageState extends State<ProductsPage> {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Column(
-          children: [
-            Card(
-              color: Colors.red,
-              child: SizedBox(
-                child: Text('hi'),
-                width: 100,
-                height: 200,
-              ),
-            ),
-          ],
-        )
-      ],
-    );
-  }
-}
+// var product = Producds(
+//     Image.asset('images/AGM/residential_system/Inverter_Cassette.jpg'),
+//     'Residential System',
+//     'Providing sustainable cooling to millions of people at home');
+
+// class _ProductsPageState extends State<ProductsPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView(
+//       children: [
+//         Column(
+//           children: [
+//             Card(
+//               child: SizedBox(
+//                 width: double.infinity,
+//                 height: 200,
+//                 child: Row(children: [
+//                   SizedBox(
+//                     width: 220,
+//                     height: double.infinity,
+//                     child: FittedBox(
+//                       child: product.image,
+//                       fit: BoxFit.fill,
+//                     ),
+//                   ),
+//                   SizedBox(
+//                     width: 40,
+//                     height: double.infinity,
+//                   ),
+//                   FittedBox(
+//                     fit: BoxFit.fill,
+//                     child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.center,
+//                       children: [
+//                         Text(
+//                           product.title,
+//                           style: TextStyle(fontSize: 20),
+//                         ),
+//                         Text(product.descreptoin)
+//                       ],
+//                     ),
+//                   )
+//                 ]),
+//               ),
+//             ),
+//           ],
+//         )
+//       ],
+//     );
+//   }
+// }
