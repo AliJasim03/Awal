2dec704d107e4356ca3a3f9811817194ed7fd3bb
kapypara <ahmed96s@hotmail.com>
2023-09-21 21:11:48 +0300

    [Home] improved on product name readability

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -4,6 +4,7 @@ import 'package:awal/constants.dart';
 import 'package:awal/models/category.dart';
 import 'package:awal/models/product.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -81,26 +82,42 @@ class HomeProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var image = ClipRRect(
-      borderRadius: const BorderRadius.only(
-        topLeft: Radius.circular(10.0),
-        topRight: Radius.circular(40.0),
-        bottomRight: Radius.circular(40.0),
-        bottomLeft: Radius.circular(40.0),
-      ), // Image border
+    const gradientStart = Colors.transparent;
+    const gradientEnd = Colors.white;
+
+    const cardShape = BorderRadius.only(
+      topLeft: Radius.circular(10.0),
+      topRight: Radius.circular(40.0),
+      bottomRight: Radius.circular(40.0),
+      bottomLeft: Radius.circular(40.0),
+    );
+
+    var prdocutCard = ClipRRect(
+      borderRadius: cardShape, // Image border
       child: GestureDetector(
         onTap: () => showActionSheet(context, product),
         child: Stack(
-          fit: StackFit.passthrough,
           children: [
-            product.image,
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 18.0),
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                product.title,
-                style: kTitleTextStyle.copyWith(fontSize: 14),
+            ShaderMask(
+              shaderCallback: (rect) {
+                return const LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [gradientStart, gradientEnd],
+                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+              },
+              blendMode: BlendMode.lighten,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: product.image.image, fit: BoxFit.fill),
+                ),
               ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              alignment: Alignment.bottomCenter,
+              child: Text(product.title, style: kTitleTextStyle.copyWith(fontSize: 14)),
             ),
           ],
         ),
@@ -108,11 +125,12 @@ class HomeProductCard extends StatelessWidget {
     );
 
     return SizedBox(
-        width: cardSize,
-        height: cardSize,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: image,
-        ));
+      width: cardSize,
+      height: cardSize,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: prdocutCard,
+      ),
+    );
   }
 }
